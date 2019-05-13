3bdb4bc3e0f6dc04097d8018704bdba000fffac2
Melanie Rogan <melanie.rogan@ft.com>
2019-05-13 16:31:23 +0100

    step eight WIP

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -1,5 +1,8 @@
 require './lib/dockingstation'
 
 describe DockingStation do 
-
+  it 'can release bike from docking station' do
+  
+  expect(DockingStation).to respond_to(release_bike)
+  end
 end

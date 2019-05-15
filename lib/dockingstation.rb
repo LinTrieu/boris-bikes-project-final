154c9446aecc10b7103fb13090e4db09c94790fc
LinTrieu <36490540+LinTrieu@users.noreply.github.com>
2019-05-15 14:21:28 +0100

    Exercise 11 - WIP

--- lib/dockingstation.rb
+++ lib/dockingstation.rb
@@ -1,9 +1,16 @@
-#require 'bike'
+require_relative 'bike'
 
 class DockingStation
 
-  def release_bike
-   return Bike.new
+  def initialize
+    @dock = []
   end
 
+  def release_bike
+   Bike.new
+  end
+
+  def dock(bike)
+    @dock.push(bike)
+  end
 end
--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -2,7 +2,7 @@ require './lib/dockingstation'
 require './lib/bike'
 
 describe DockingStation do
-  it 'can release bike from docking station' do
+  it 'releases bike from docking station' do
     docking_station = DockingStation.new
     expect(docking_station.release_bike).to be_an_instance_of(Bike)
   end
@@ -10,12 +10,20 @@ describe DockingStation do
   it 'expects the bike to be working' do
     docking_station = DockingStation.new
     released_bike = docking_station.release_bike
-    expect(released_bike.working?).to eq(true)
+    expect(released_bike).to be_working
   end
 
-  it 'expects to return the bike to docking station' do
+  it 'can dock a bike' do
     docking_station = DockingStation.new
-    expect(docking_station.return_bike).to eq(true)
+    bike = Bike.new
+    output = docking_station.dock(bike)
+    expect(output).to include(bike)
   end
 
+    # it 'returns docked bikes' do
+    #   docking_station = DockingStation.new
+    #   bike = Bike.new
+    #   output = docking_station.dock(bike)
+    #   expect(output).to eq(dock)
+    # end
 end

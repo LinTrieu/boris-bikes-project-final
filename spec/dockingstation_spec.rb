6d8009497df32725cba9463c6d564e092cd5b4d5
Ollie Porter <ol.porter@me.com>
2019-05-16 15:09:57 +0100

    Refactoring to use lets for docking_station & bike

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -2,39 +2,33 @@ require './lib/dockingstation'
 require './lib/bike'
 
 describe DockingStation do
+
+  let(:docking_station) {DockingStation.new}
+  let(:bike) {Bike.new}
+
   it 'releases bike from docking station' do
-    docking_station = DockingStation.new
-    bike = Bike.new
     docking_station.dock(bike) # => total num of bikes should be 1
     expect(docking_station.release_bike).to be_an_instance_of(Bike)
   end
 
   it 'expects the bike to be working' do
-    docking_station = DockingStation.new
-    bike = Bike.new
     docking_station.dock(bike) # => total num of bikes should be 1
     output = docking_station.release_bike
     expect(output).to be_working
   end
-  
+
 
   it 'can dock a bike' do
-    docking_station = DockingStation.new
-    bike = Bike.new
     output = docking_station.dock(bike)
     expect(output).to eq(1)
   end
 
   it 'can iteract with a method called total' do
-    docking_station = DockingStation.new
-    bike = Bike.new
     expect(docking_station).to respond_to(:total)
   end
 
   it 'can add bikes to the docking station total' do
     #arrange
-    docking_station = DockingStation.new
-    bike = Bike.new
     #assert
     output = 5.times {docking_station.dock(bike)}
     #assertion
@@ -43,14 +37,11 @@ describe DockingStation do
 
   it 'expects docking station to respond to release bike method' do
     #arrange
-    docking_station = DockingStation.new 
     #assertion
     expect(docking_station).to respond_to(:release_bike)
   end
 
   it 'can remove bike from the docking station total' do
-    docking_station = DockingStation.new
-    bike = Bike.new
     docking_station.dock(bike)
     docking_station.dock(bike)
     docking_station.release_bike
@@ -58,8 +49,6 @@ describe DockingStation do
   end
 
   it 'can raise an error when there are no bikes docked' do
-    docking_station = DockingStation.new
-    bike = Bike.new
     docking_station.dock(bike)
     p "******"
     p docking_station.total #=> 1

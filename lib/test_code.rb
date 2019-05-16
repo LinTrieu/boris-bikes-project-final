d6cd98cd4c3dc69fd1414e25baede92dc240aee3
Ollie Porter <ol.porter@me.com>
2019-05-16 09:45:17 +0100

    Played around with the test file to output the code we wanted

--- lib/test_code.rb
+++ lib/test_code.rb
@@ -4,5 +4,5 @@ require_relative 'bike'
 dockingstation = DockingStation.new
 bike = dockingstation.release_bike
 dockingstation.dock(bike)
-dockingstation.dock(bike) # => Docking bike, we're returning bike PASSES
+dockingstation.dock(bike)
 puts dockingstation.total
